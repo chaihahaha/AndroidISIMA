c4ae4632e648ed97a19d6b07ba74f48f98068a7c
chaihahaha <836275709@qq.com>
2020-03-23 22:57:09 +0100

    Basis

--- app/src/main/java/com/example/androidisima/MainActivity.java
+++ app/src/main/java/com/example/androidisima/MainActivity.java
@@ -3,6 +3,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
@@ -11,14 +12,15 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = (TextView) findViewById(R.id.textView);
+        //TextView textView = (TextView) findViewById(R.id.textView);
         Button button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 TextView textView = v.getRootView().findViewById(R.id.textView);
+                EditText editText = v.getRootView().findViewById(R.id.editText);
                 if(textView!=null)
                 {
-                    textView.setText("Hello world");
+                    textView.setText(editText.getText());
                 }
             }
         });

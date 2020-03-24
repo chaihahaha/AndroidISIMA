58ae10600a22c3e5ad25b4164c7e88a8fe35ccd5
chaihahaha <836275709@qq.com>
2020-03-24 02:05:59 +0100

    Test OkHttp

--- app/src/main/java/com/example/androidisima/MainActivity.java
+++ app/src/main/java/com/example/androidisima/MainActivity.java
@@ -6,21 +6,42 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TextView textView = (TextView) findViewById(R.id.textView);
-        Button button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                TextView textView = v.getRootView().findViewById(R.id.textView);
-                EditText editText = v.getRootView().findViewById(R.id.editText);
-                if(textView!=null)
-                {
-                    textView.setText(editText.getText());
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url("https://catfact.ninja/fact").build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if(!response.isSuccessful()){
+                    throw new IOException(response.toString());
+                } else {
+                    ResponseBody responseBody = response.body();
+                    if(responseBody != null) {
+                        textView.setText(responseBody.string());
+                    }
+
                 }
             }
         });

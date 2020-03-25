76afcb277d23d909eb6218a43112c7c76b5a366e
chaihahaha <836275709@qq.com>
2020-03-25 19:34:44 +0100

    Image Use picasso

--- app/src/main/java/com/example/androidisima/FactDetailActivity.java
+++ app/src/main/java/com/example/androidisima/FactDetailActivity.java
@@ -1,12 +1,25 @@
 package com.example.androidisima;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
+
+import java.util.Arrays;
+
 public class FactDetailActivity extends AppCompatActivity {
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +28,23 @@ public class FactDetailActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
+        ImageView imageView = findViewById(R.id.imageView);
+        assert message != null;
+        String[] first5words = Arrays.copyOfRange(message.split(" ", 200),0,5);
+        String words = String.join(" ", first5words)+"...";
+        System.out.println(words);
+        RequestCreator rc = Picasso.get().load("https://cataas.com/cat/says/"+words);
+        final ProgressBar progressBar1 = findViewById(R.id.progressBar1);
+        rc.into(imageView, new Callback() {
+            @Override
+            public void onSuccess() {
+                progressBar1.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onError(Exception e) {
+
+            }
+        });
     }
 }
